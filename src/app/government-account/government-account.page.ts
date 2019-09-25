7f4a8bb479bdce62c78f1a85ab76bec80a9d28d2
Thannarot <thannarotk@gmail.com>
2019-09-25 16:33:33 +0700

    fixed minor

--- src/app/government-account/government-account.page.ts
+++ src/app/government-account/government-account.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { TranslateConfigService } from '../translate-config.service';
 @Component({
   selector: 'app-government-account',
   templateUrl: './government-account.page.html',
   styleUrls: ['./government-account.page.scss'],
 })
-export class GovernmentAccountPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class GovernmentAccountPage {
+    selectedLanguage:string;
+    constructor(private translateConfigService: TranslateConfigService){
+      this.selectedLanguage = this.translateConfigService.getDefaultLanguage();
+    }
+    languageChanged(){
+      this.translateConfigService.setLanguage(this.selectedLanguage);
+    }
 
 }
